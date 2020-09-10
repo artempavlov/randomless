07e0dcd0301536037f8717cf05188420eed46053
artempavlov <art.pavlov.2012@gmail.com>
2020-09-10 17:16:56 +0300

    added exit method, requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import numpy as np
 import cv2
-import 
+
 
 class RandomGenerator(object):
     def __init__(self):
@@ -15,28 +14,15 @@ class RandomGenerator(object):
 
 class CameraRandom(RandomGenerator):
     def __init__(self):
-        self.cap = cv2.VideoCapture(0)
-        
-        #while(True):
-            # Capture frame-by-frame
-        #    ret, frame = cap.read()
-        
-            # Our operations on the frame come here
-            #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
-            # Display the resulting frame
-        #    cv2.imshow('frame',gray)
-            #if cv2.waitKey(1) & 0xFF == ord('q'):
-            #    break
-        
-        # When everything done, release the capture
-        #cap.release()
-        #cv2.destroyAllWindows()
+        self.cap = cv2.VideoCapture(1)
     
     def random(self):
         ret, frame = self.cap.read()
         if ret:
-            return frame[0][0][0] / 255
+            return frame.sum() % 10
+    
+    def exit(self):
+        self.cap.release()
 
 
 if __name__ == '__main__':
