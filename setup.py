f245cea09744d76c7100bd2a965f6760501ac008
artempavlov <art.pavlov.2012@gmail.com>
2020-09-13 13:29:19 +0300

    changed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="randomless",
-    version="0.0.5",
+    version="0.0.6",
     author="Artem Pavlov",
     description="A True Random Number Generator for python.",
     long_description=long_description,
